8e4be519bf02d1b6827dfff0341e0096e7cb34ad
kankyouhakai <kankyouhakai096@gmail.com>
2024-03-31 23:23:42 +0900

    treesitterを使えるようになった
    optsではなく，config function()の中に書かなければならなかった

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -3,12 +3,22 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	lazy = true,
 	event = "BufRead",
-	opts = {
-		ensure_installed = { "c", "cpp", "lua", "typescript", "python", "javascript" },
-		highlight = {
-			enable = true,
-			additional_vim_regex_highlighting = false,
-		},
-		sync_install = false,
-	},
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+
+		 configs.setup({
+          ensure_installed = { "c", "cpp", "lua", "vim", "javascript", "html", "typescript", "markdown" },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },  
+        })
+	end
+	--opts = {
+	--	ensure_installed = { "c", "cpp", "lua", "typescript", "python", "javascript" },
+	----	highlight = {
+	--		enable = true,
+	--		additional_vim_regex_highlighting = false,
+	--	},
+	--	sync_install = false,
+	--}--,
 }
