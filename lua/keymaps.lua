887a20e24e51cdc5d4f5c66c1c68a18d0f26e888
kankyouhakai <kankyouhakai096@gmail.com>
2024-04-26 15:48:24 +0900

    vim最高とあなたも言いなさい

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -19,6 +19,7 @@ keymap("n", "<ESC><ESC>", "<CMD>set nohlsearch<CR>", opts)
 --補完表示時のEnterで改行をしない
 --keymap("i", "<CR>", "pumvisible() ? '<C-y>' : '<CR>'", { expr = true })
 keymap("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', {silent = true, noremap = true, expr = true, replace_keycodes = false})
+keymap("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], {silent = true, noremap = true, expr = true, replace_keycodes = false})
 
 keymap("n", "V", "<C-v>", opts) --ビジュアルモード
 
